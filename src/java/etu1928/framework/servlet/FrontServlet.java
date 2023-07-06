9468bc13535ccc4a7ad87102382a4d12130cc131
Pakito033 <119033755+Pakito033@users.noreply.github.com>
2023-07-06 19:44:56 -0400

    sprint3

--- src/java/etu1928/framework/servlet/FrontServlet.java
+++ src/java/etu1928/framework/servlet/FrontServlet.java
@@ -6,13 +6,24 @@
 package etu1928.framework.servlet;
 
 import etu1928.framework.Mapping;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
+import annotation.AnnotationUrl;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,6 +32,84 @@ import javax.servlet.http.HttpServletResponse;
 public class FrontServlet extends HttpServlet {
     
     HashMap<String, Mapping> MapppingUrls;
+
+    public HashMap<String, Mapping> getMapppingUrls() {
+        return MapppingUrls;
+    }
+
+    public void setMapppingUrls(HashMap<String, Mapping> MapppingUrls) {
+        this.MapppingUrls = MapppingUrls;
+    }
+    
+    public ArrayList<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+        ArrayList<Class> classes = new ArrayList<Class>();
+        if (!directory.exists())
+        {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files)
+        {
+            if (file.isDirectory())
+            {
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            }
+            else if (file.getName().endsWith(".class"))
+            {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
+        }
+        return classes;
+    }
+
+    public ArrayList<Class> getClasses(String packageName) throws ClassNotFoundException, IOException, URISyntaxException{
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        System.out.println(resources.hasMoreElements());
+        ArrayList<File> dirs = new ArrayList<File>();
+        while (resources.hasMoreElements())
+        {
+            URL resource = resources.nextElement();
+            URI uri = new URI(resource.toString());
+            dirs.add(new File(uri.getPath()));
+        }
+        ArrayList<Class> classes = new ArrayList<Class>();
+        System.out.println("eto zao");
+        for (File directory : dirs)
+        {
+            classes.addAll(findClasses(directory, packageName));
+        }
+
+        return classes;
+    }
+
+    public FrontServlet() {
+    }
+
+    @Override
+    public void init() throws ServletException{
+        HashMap<String,Mapping> temp = new HashMap<String,Mapping>();
+        try{
+            ArrayList<Class> list = getClasses("etu1928");
+            for(Class element : list){
+               Method[] methods = element.getDeclaredMethods();
+               for(Method m : methods){
+                   if(m.isAnnotationPresent(AnnotationUrl.class)){
+                       AnnotationUrl annotation = m.getAnnotation(AnnotationUrl.class);
+                       temp.put(annotation.url(),new Mapping(element.getName(),m.getName())); 
+                   }
+               }
+            }
+            this.setMapppingUrls(temp);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
