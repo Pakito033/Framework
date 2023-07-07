c9990c4800f7440d748f15273cea1bc62871c76f
Pakito033 <119033755+Pakito033@users.noreply.github.com>
2023-07-07 17:02:33 -0400

    sprint5

--- Framework/src/java/etu1928/framework/Mapping.java
+++ Framework/src/java/etu1928/framework/Mapping.java
@@ -10,5 +10,32 @@ package etu1928.framework;
  * @author Tim
  */
 public class Mapping {
+    String className;
+    String Method;
+
+    public Mapping() {
+    }
+
+    public Mapping(String className, String Method) {
+        this.className = className;
+        this.Method = Method;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethod() {
+        return Method;
+    }
+
+    public void setMethod(String Method) {
+        this.Method = Method;
+    }
+    
     
 }
---
+++ Framework/src/java/etu1928/framework/ModelView.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etu1928.framework;
+
+/**
+ *
+ * @author Tim
+ */
+public class ModelView {
+    String url;
+
+    public ModelView() {
+    }
+
+    public ModelView(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    
+}
